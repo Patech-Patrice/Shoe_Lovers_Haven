4b049b47c4fb04a4227559d0d4b4077f130c38fc
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-03-06 13:35:54 -0500

    added associations and modified migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password #accessed through bcrypt which allows us to use the authenticate method
+  has_many :shoe_entries
 end
--- db/migrate/20190306181310_create_shoe_entries.rb
+++ db/migrate/20190306181310_create_shoe_entries.rb
@@ -5,5 +5,6 @@ class CreateShoeEntries < ActiveRecord::Migration[5.2]
       t.integer :brand_id
       t.integer :style_id
       t.integer :user_id
+      t.timestamps null: false
   end
 end
