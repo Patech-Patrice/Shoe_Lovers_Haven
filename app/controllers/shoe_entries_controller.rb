16de91d4c97ed39aa2c5644339c6a56f04da666d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-03-11 01:11:06 +0000

    created post route for new shoe entry

--- app/controllers/shoe_entries_controller.rb
+++ app/controllers/shoe_entries_controller.rb
@@ -6,7 +6,22 @@ class ShoeEntriesController < ApplicationController
     erb :'/shoe_entries/new'
   end
 
-  # post request to create a new shoe entry
+  # post request to create a new shoe entry and save it to the database
+  # only want to save entries with content
+  # only create an entry if the user is logged in
+  post '/shoe_entries' do
+    if !logged_in?
+      redirect '/'
+    end
+    #Only show entries with content
+      if params[:name] != "" && params[:brand_id] != "" && params[:style_id] != ""
+        #create a new entry
+        @shoe_entry = ShoeEntry.create(name: params[:name], brand_id: params[:brand_id], style_id: params[:style_id], user_id: current_user.id)
+        redirect to "/shoe_entries/#{@shoe_entry.id}"
+    else
+      redirect to '/shoe_entries/new'
+  end
+end
 
 
   #show route shoe entry
