f6c7f4de0607a8456eec0bc35a7a9c55404cf6a0
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-03-14 22:28:14 +0000

    updated shoe_entries_controller shoe_entries route

--- app/controllers/shoe_entries_controller.rb
+++ app/controllers/shoe_entries_controller.rb
@@ -15,18 +15,18 @@ class ShoeEntriesController < ApplicationController
   # only want to save entries with content
   # only create an entry if the user is logged in
   post '/shoe_entries' do
-    if !logged_in?
+    if !logged_in? && params[:name] != ""
       redirect '/'
-    end
+    else
     #Only show entries with content
-      if params[:name] != "" && params[:brand_id] != "" && params[:style_id] != ""
+      #if params[:name] != "" && params[:brand_id] != "" && params[:style_id] != ""
         #create a new entry
         @shoe_entry = ShoeEntry.create(name: params[:name], brand_id: params[:brand_id], style_id: params[:style_id], user_id: current_user.id)
         redirect to "/shoe_entries/#{@shoe_entry.id}"
-    else
+    end
       redirect to '/shoe_entries/new'
   end
-end
+
 
 
   #show route shoe entry; dynamic route
